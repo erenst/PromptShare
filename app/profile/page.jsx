d801d58dbc2259881139b8326718157115da9a1e
erenst <erenst2002@hotmail.com>
2023-07-23 15:28:50 +0930

    complet edit/delete prompt

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,6 +6,7 @@ import Profile from "@components/Profile";
 const ProfilePage = () => {
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
+  const router = useRouter();
   useEffect(() => {
     if (!session) return;
     const fetchPrompts = async () => {
@@ -19,8 +20,25 @@ const ProfilePage = () => {
     };
     if (session?.user.id) fetchPrompts();
   }, [session?.user.id]);
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <Profile
       name="My"
